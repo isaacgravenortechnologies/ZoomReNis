7c9712cb0da6543b51288e75100a0726dd7df9ca
isaacgravenortechnologies <isaac.gravenor@onlinediscoveries.net>
2024-01-06 22:34:01 +0000

    Added beginning foundations for script and gitignored script temp output

---
+++ extractAssets.py
@@ -0,0 +1,14 @@
+import urllib.request
+import zipfile
+import os
+print("DO NOT USE! This script is incomplete and may produce unexpected, memory-using or storage eating output.")
+exit()
+installDir=input("Please enter the directory where your Zoombinis Steam install is (Zoombinis.exe): ")
+print("Downloading FFmpeg")
+urllib.request.urlretrieve("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip", "./temp/ffmpeg-release-essentials.zip")
+print("Extracting FFmpeg")
+with zipfile.ZipFile("./temp/ffmpeg-release-essentials.zip", 'r') as zip_ref:
+    zip_ref.extractall("./temp/")
+root="./temp/"
+dirlist = [ item for item in os.listdir(root) if os.path.isdir(os.path.join(root, item)) ]
+os.system(os.getcwd()+"/temp/"+dirlist[0]+"/bin/ffmpeg.exe -i "+installDir+" -r 1/1 frame%04d.jpg")
