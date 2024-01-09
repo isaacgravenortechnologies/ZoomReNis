e08538cda4cb207eb823d6bc7def7fdceb53f540
isaacgravenortechnologies <isaac.gravenor@onlinediscoveries.net>
2024-01-09 22:59:44 +0000

    Added to multi OS support in extract script

--- extractAssets.py
+++ extractAssets.py
@@ -1,18 +1,22 @@
 import urllib.request
 import zipfile
 import os
+def downloadFFmpeg(os):
+    if(os=="w"):
+        print("Downloading FFmpeg")
+        urllib.request.urlretrieve("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip", "./temp/ffmpeg-release-essentials.zip")
+        print("Extracting FFmpeg")
+        with zipfile.ZipFile("./temp/ffmpeg-release-essentials.zip", 'r') as zip_ref:
+            zip_ref.extractall("./temp/")
+        root="./temp/"
+        dirlist = [ item for item in os.listdir(root) if os.path.isdir(os.path.join(root, item)) ]
+        os.system(os.getcwd()+"/temp/"+dirlist[0]+"/bin/ffmpeg.exe -i "+installDir+" -r 1/1 frame%04d.jpg")
+    if(os=="l"):
+        print("Downloading FFmpeg")
+        urllib.request.urlretrieve("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip", "./temp/ffmpeg-release-essentials.zip")
+
 print("DO NOT USE! This script is incomplete and may produce unexpected, memory-using or storage eating output.")
 exit()
 platform=input("Please enter your platform: 'w' for Windows, 'm' for MacOS or 'l' for Linux. other OSes are not currently supported, maybe raise an issue on GitHub.")
-if(platform.lower()=="w"):
-    installDir=input("Please enter the directory where your Zoombinis Steam install is (Zoombinis.exe): ")
-    print("Downloading FFmpeg")
-    urllib.request.urlretrieve("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip", "./temp/ffmpeg-release-essentials.zip")
-    print("Extracting FFmpeg")
-    with zipfile.ZipFile("./temp/ffmpeg-release-essentials.zip", 'r') as zip_ref:
-        zip_ref.extractall("./temp/")
-    root="./temp/"
-    dirlist = [ item for item in os.listdir(root) if os.path.isdir(os.path.join(root, item)) ]
-    os.system(os.getcwd()+"/temp/"+dirlist[0]+"/bin/ffmpeg.exe -i "+installDir+" -r 1/1 frame%04d.jpg")
-elif(platform.lower()=="l"):
-    print("Downloading FFmpeg")
+installDir=input("Please enter the directory where your Zoombinis Steam install is (Zoombinis.exe/other executable format): ")
+downloadFFmpeg(platform.lower())
